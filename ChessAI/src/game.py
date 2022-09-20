0285ab9d800ceba246b431f132c733819aef545e
JC Tapias <jctblock@gmail.com>
2022-09-20 11:55:19 -0400

    Checkpoint

--- ChessAI/src/game.py
+++ ChessAI/src/game.py
@@ -1,8 +1,13 @@
-#
+import pygame
+
+from const import *
+
 class Game:
 
     def __init__(self):
         pass
 
     def show_bg(self, surface):
-        pass
+        for row in range(ROWS):
+            for col in range(COLS):
+                
