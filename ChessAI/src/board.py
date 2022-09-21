b7c95b4c7eaf1febe898d44d8857ecac90baf741
JC Tapias <jctblock@gmail.com>
2022-09-21 18:08:00 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -6,7 +6,11 @@ class Board:
         self.squares = []
 
     def _create(self):
+
         self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.squares[row][col]
 
     def _add_pieces(self, color):
         pass
