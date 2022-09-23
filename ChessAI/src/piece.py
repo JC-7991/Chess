21d2f7d9a1b17e6951606789b1ff3c1a53924c90
JC Tapias <jctblock@gmail.com>
2022-09-23 00:00:23 -0400

    Checkpoint

--- ChessAI/src/piece.py
+++ ChessAI/src/piece.py
@@ -5,16 +5,31 @@ class Piece:
 
 class Pawn(Piece):
 
-    def _init_(self, color):  
+    def __init__(self, color):  
         self.dir = -1 if color == 'white' else 1
-        super().__init('pawn', color, 1.0)
+        super().__init__('pawn', color, 1.0)
 
 class Knight(Piece):
 
-    def _init_(self, color):  
-        super().__init('knight', color, 3.0)
+    def __init__(self, color):  
+        super().__init__('knight', color, 3.0)
 
 class Bishop(Piece):
 
-    def _init_(self, color):  
-        super().__init('bishop', color, 3.001)
+    def __init__(self, color):  
+        super().__init__('bishop', color, 3.001)
+
+class Rook(Piece):
+
+    def __init__(self, color):  
+        super().__init__('rook', color, 5.0)
+
+class Queen(Piece):
+
+    def __init__(self, color):  
+        super().__init__('queen', color, 9.0)
+
+class King(Piece):
+
+    def __init__(self, color):  
+        super().__init__('king', color, 10000.0)
