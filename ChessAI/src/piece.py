3b003568e4c2c62b0bdccb5d44cfbfcdc8a38ec1
JC Tapias <jctblock@gmail.com>
2022-09-24 00:03:00 -0400

    Checkpoint

--- ChessAI/src/piece.py
+++ ChessAI/src/piece.py
@@ -1,3 +1,5 @@
+import os
+
 class Piece:
 
     def __init__(self, name, color, value, texture = None, texture_rect = None):
@@ -11,8 +13,10 @@ class Piece:
         self.set_texture()
         self.texture_rect = texture_rect
 
-    def set_texture(self):
-        pass
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+           f'assets/images/imgs-{size}px' 
+        )
 
 
 class Pawn(Piece):
