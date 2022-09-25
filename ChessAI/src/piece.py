cc4523a0d9572206385089d3ce251664ada68f0c
JC Tapias <jctblock@gmail.com>
2022-09-25 00:00:06 -0400

    Checkpoint

--- ChessAI/src/piece.py
+++ ChessAI/src/piece.py
@@ -14,10 +14,10 @@ class Piece:
         self.texture_rect = texture_rect
 
     def set_texture(self, size = 80):
+        self.texture = os.path.join(f'assets/images/imgs-{size}px/{self.color}_{self.name}.png')
 
-        self.texture = os.path.join(
-            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
-        )
+    def add_moves(self, move):
+        pass
 
 
 class Pawn(Piece):
