54cdc9b0297c1f9f7da91f0e0b75ec0c1cf146d7
JC Tapias <jctblock@gmail.com>
2022-09-26 12:51:29 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -18,5 +18,6 @@ class Board:
     def _add_pieces(self, color):
 
         row_pawn, row_other = (6, 7) if color == 'white' else (1, 0)
+        
         for col in range(COLS):
             self.squares[row_other][col] = Square(row_pawn, col, Pawn(color)) 
