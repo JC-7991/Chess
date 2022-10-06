dbf492fcfbdbf49d5faef4f4bfceba93ac571730
JC Tapias <jctblock@gmail.com>
2022-10-06 16:02:55 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -18,7 +18,7 @@ class Board:
             possible_moves = [
                 (row - 2, col + 1),
                 (row - 1, col + 2),
-                (),
+                (row + 1, col + 2),
                 (),
                 (),
                 (),
