9cb19816b7a1a7e7d590f565b9a47f6b139a0593
JC Tapias <jctblock@gmail.com>
2022-10-06 16:06:12 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -21,11 +21,11 @@ class Board:
                 (row - 1, col + 2),
                 (row + 1, col + 2),
                 (row + 2, col + 1),
-                
+
                 (row + 2, col - 1),
                 (row + 1, col - 2),
                 (row - 1, col - 2),
-                (row - 2, col - 1)
+                (row - 2, col - 1),
 
             ]
         
