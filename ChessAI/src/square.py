96029cd245f3e1231a617449714e6edf457f92af
JC Tapias <jctblock@gmail.com>
2022-10-08 18:33:34 -0400

    Checkpoint

--- ChessAI/src/square.py
+++ ChessAI/src/square.py
@@ -10,6 +10,8 @@ class Square:
 
     @staticmethod
     def in_range(*args):
+        
         for arg in args:
             if arg < 0 or arg > 7:
                 return False
+        return True
