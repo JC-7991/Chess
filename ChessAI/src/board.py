2ac5c2d4578b25a8b2e45ccdeaadaccaa40ed66b
JC Tapias <jctblock@gmail.com>
2022-10-19 22:01:24 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -107,7 +107,7 @@ class Board:
 
                 possible_move_row, possible_move_col = possible_move
                 if Square.in_range(possible_move_row, possible_move_col):
-                    if self.squares[possible_move_row][possible_move_col]:
+                    if self.squares[possible_move_row][possible_move_col].isempty_or_enemy(piece.color):
                         pass
         
         if isinstance(piece, Pawn):
