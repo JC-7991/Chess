c02043ab3600cd89e18f037930f0ab36bca04e72
JC Tapias <jctblock@gmail.com>
2022-10-22 11:30:06 -0400

    Checkpoint

--- ChessAI/src/main.py
+++ ChessAI/src/main.py
@@ -1,8 +1,10 @@
 import pygame
 import sys
+
 from const import *
 from game import Game
 from square import Square
+from move import Move
 
 class Main:
 
@@ -64,10 +66,14 @@ class Main:
                 elif event.type == pygame.MOUSEBUTTONUP:
 
                     if dragger.dragging:
+
                         dragger.update_mouse(event.pos)
                         released_row = dragger.mouseY // SQSIZE
                         released_col = dragger.mouseX // SQSIZE
+
                         initial = Square(dragger.initial_row, dragger.initial_col)
+                        final = Square(released_row, released_col)
+                        move = Move(initial, final)
 
                     dragger.undrag_piece()
 
