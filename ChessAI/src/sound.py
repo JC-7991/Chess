b79a2ebb6fa94f5de174fc9197802d3d7b86e4eb
JC Tapias <jctblock@gmail.com>
2022-10-25 17:21:49 -0400

    Checkpoint

--- ChessAI/src/sound.py
+++ ChessAI/src/sound.py
@@ -2,8 +2,9 @@ import pygame
 
 class Sound:
     
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
+        self.sound = pygame.mixer.Sound()
 
     def play(self):
         pass
