30414a8ecf59d647b68176caf6b782c7be733cc5
JC Tapias <jctblock@gmail.com>
2022-10-26 16:40:49 -0400

    Checkpoint

--- ChessAI/src/config.py
+++ ChessAI/src/config.py
@@ -6,11 +6,12 @@ from theme import Theme
 class Config:
 
     def __init__(self):
-        
+
         self.themes = []
         self._add_themes()
         self.idx = 0
         self.theme = self.themes[self.idx]
+        self.move_sound = Sound()
 
     def change_theme():
         pass
