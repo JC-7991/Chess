fce57c0bdf7bed932bebee7237ee3892557f8b23
JC Tapias <jctblock@gmail.com>
2022-10-29 16:10:32 -0400

    Checkpoint

--- ChessAI/src/square.py
+++ ChessAI/src/square.py
@@ -37,4 +37,5 @@ class Square:
 
     @staticmethod
     def get_alphacol(col):
-        pass
+        ALPHACOLS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
+        return ALPHACOLS[col]
