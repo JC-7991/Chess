dd2f41a1627034bbe220af70981b9ae68c42ec1d
JC Tapias <jctblock@gmail.com>
2022-10-29 22:23:07 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -35,6 +35,9 @@ class Board:
     def check_promotion(self, piece, final):
         if final.row == 0 or final.row == 7:
             self.squares[final.row][final.col].piece = Queen(piece.color)
+    
+    def castling(self, initial, final):
+        pass
 
     def calc_moves(self, piece, row, col):
 
