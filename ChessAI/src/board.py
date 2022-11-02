ef563f38a9568fc8a4a9c345714876cb35b0f414
JC Tapias <jctblock@gmail.com>
2022-11-02 15:18:15 -0400

    Checkpoint

--- ChessAI/src/board.py
+++ ChessAI/src/board.py
@@ -29,6 +29,7 @@ class Board:
             if self.castling(initial, final):
                 diff = final.col - initial.col
                 rook = piece.left_rook if (diff < 0) else piece.right_rook
+                self.move(rook, rook.moves[-1])
 
         piece.moved = True
         piece.clear_moves()
